8718f93f6e5e0b03db68d38e80820c5da07402cc
vab2048 <vab2048@gmail.com>
2026-03-24 16:30:27 +0000

    Add tailscale installation files.

---
+++ install/tailscale/v24.04.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+#######################################################################################################################
+# Install tailscale
+# We just source the full install script.
+#######################################################################################################################
+script_path="$(realpath "$0")"
+script_dir="$(dirname "$script_path")"
+
+source "$script_dir/install.sh"
