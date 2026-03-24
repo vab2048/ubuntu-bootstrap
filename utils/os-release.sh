28cddcd9884b0d1140033646abcfedc6079ca26c
vab2048 <vab2048@gmail.com>
2026-03-24 15:32:51 +0000

    Add utils/os-release.sh script to centralize commentary on sourcing the /etc/os-release file

---
+++ utils/os-release.sh
@@ -0,0 +1,7 @@
+# This single command is kept here so that commentary on it is centralized.
+########
+# Source /etc/os-release to get a number of env vars. For noble (24.04), version related env vars included:
+# PRETTY_NAME="Ubuntu 24.04.4 LTS"       ;   NAME="Ubuntu"           ; VERSION_ID="24.04"            ;
+# VERSION="24.04.4 LTS (Noble Numbat)"   ;   VERSION_CODENAME=noble  ; UBUNTU_CODENAME=noble         ;
+source /etc/os-release
+########
