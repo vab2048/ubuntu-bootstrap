8e931c1fcf33a43bc0fe66225b50f5aadb875dd6
vab2048 <vab2048@gmail.com>
2026-03-30 13:43:06 +0100

    Add ufw install script.

---
+++ module/ufw/v24.04-install.sh
@@ -0,0 +1,8 @@
+
+#######################################################################################################################
+# Install ufw
+# - It is likely that ufw is already installed - it comes by default on both server and desktop Ubuntu installations.
+# - "ubuntu-minimal" probably does not ship it.
+#######################################################################################################################
+
+sudo apt-get install -y ufw
